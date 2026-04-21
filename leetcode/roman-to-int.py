7768fad97aec58b08b176766e0cf2c10e07dd8a7
Piotr Kubski <piotrkubski3@gmail.com>
2026-04-21 21:40:11 +0200

    lc roman to int

---
+++ leetcode/roman-to-int.py
@@ -0,0 +1,20 @@
+def romanToInt(s):
+    roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    i = 0
+    result = 0
+    while i < len(s):
+        if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+            result += (roman[s[i + 1]] - roman[s[i]])
+            i += 2
+        else:
+            result += roman[s[i]]
+            i += 1
+    return result
+
+    # result = 0
+    # for i in range(len(s)):
+    #     if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+    #         result -= roman[s[i]]
+    #     else:
+    #         result += roman[s[i]]
+    # return result
